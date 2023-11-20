6e4f872e7f9c2dbe62f2b81cfabc004c136c7a90
dimasm93 <software.dimas_m@icloud.com>
2023-11-20 14:47:45 +0700

    add base module

---
+++ order-service/src/main/java/com/maryanto/dimas/example/App.java
@@ -0,0 +1,11 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
